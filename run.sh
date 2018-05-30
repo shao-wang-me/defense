2e2709d27ff64430a1855e81ea8b3f5d22632df0
Shao Wang <wsdonny@gmail.com>
2018-05-30 22:46:44 +1000

    new

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-(../bin HFO --offense-agents 1 --defense-npcs 2 --headless --fullstat --seed 123 &) && python ./agent.py
+(../bin/HFO --offense-agents 1 --defense-npcs 2 --headless --fullstat --seed 123 &) && python ./agent.py
