0e5cc82206380f37f4dca98e1c3fc0f804a971a2
Shao Wang <wsdonny@gmail.com>
2018-05-30 22:40:11 +1000

    new

--- test.py
+++ test.py
@@ -1,14 +1,26 @@
-from keras.models import Sequential
+from keras.models import Sequential, Input, Model
 from keras.layers import Dense
 import numpy as np
 
-model = Sequential()
-model.add(Dense(units=4, input_dim=1))
-model.add(Dense(units=4))
-model.add(Dense(units=2))
-model.compile(loss='mean_squared_error', optimizer='adam')
+inputs = Input(shape=(1,))
+x = Dense(units=4)(inputs)
+x = Dense(units=4)(x)
+predictions1 = Dense(units=1)(x)
+predictions2 = Dense(units=1)(x)
+model1 = Model(inputs=inputs, outputs=predictions1)
+model2 = Model(inputs=inputs, outputs=predictions2)
+model1.compile(loss='mean_squared_error', optimizer='adam')
+model2.compile(loss='mean_squared_error', optimizer='adam')
+model1.summary()
+model2.summary()
 
-model.summary()
+# model = Sequential()
+# model.add(Dense(units=4, input_dim=1))
+# model.add(Dense(units=4))
+# model.add(Dense(units=2))
+# model.compile(loss='mean_squared_error', optimizer='adam')
+
+# model.summary()
 
 # from keras.utils import plot_model
 # plot_model(model, to_file='model.png')
@@ -20,16 +32,17 @@ model.summary()
 x_train = np.fromfunction(lambda i: i, (10000,), dtype=int)
 y_train1 = np.fromfunction(lambda i: 3 * i + 8, (10000,), dtype=int)
 y_train2 = np.fromfunction(lambda i: 3 * i + 18, (10000,), dtype=int)
-y_train = np.column_stack((y_train1, y_train2))
-model.fit(x_train, y_train, epochs=500, batch_size=32)
-
+model1.fit(x_train, y_train1, epochs=500, batch_size=32)
+model2.fit(x_train, y_train2, epochs=500, batch_size=32)
 
 x_test = np.fromfunction(lambda i: i, (10099,), dtype=int)[10000:]
 y_test1 = np.fromfunction(lambda i: 3 * i + 8, (10099,), dtype=int)[10000:]
 y_test2 = np.fromfunction(lambda i: 3 * i + 18, (10099,), dtype=int)[10000:]
-y_test = np.column_stack((y_test1, y_test2))
-loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
+loss_and_metrics1 = model1.evaluate(x_test, y_test1, batch_size=128)
+loss_and_metrics2 = model1.evaluate(x_test, y_test2, batch_size=128)
 
-print(loss_and_metrics)
+print(loss_and_metrics1)
+print(loss_and_metrics2)
 
-print(model.predict(np.array([1,2,3,4,5,6,7,8,9,10])))
+print(model1.predict(np.array([1,2,3,4,5,6,7,8,9,10])))
+print(model2.predict(np.array([1,2,3,4,5,6,7,8,9,10])))
